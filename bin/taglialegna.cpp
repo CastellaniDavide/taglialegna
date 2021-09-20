becdd396a598fc2a59bd16ef3cd589c16f7e314a
createstructure <contacts@castellanidavide.it>
2021-09-20 15:32:11 +0000

    createstructure

---
+++ bin/taglialegna.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file taglialegna.cpp
+ *
+ * @version 01.01 time_now
+ *
+ * @brief 
+ *
+ * @ingroup taglialegna
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
